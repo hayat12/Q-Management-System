772626f4321829c39cccc13f19319d33a935762e
Hayatullah Rahnamoon <rahnamoonhayatullah@gmail.com>
2019-02-03 02:58:08 +0800

    update queue users

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -14,6 +14,8 @@ export class HomeComponent implements OnInit {
   isNewUser:boolean = false;
   isMember:boolean = false;
 
+  p_num = ['000','001','002','003','004','005','006','007','008','009','010','011','012']
+
   constructor(
     private fb: FormBuilder,
     private router: Router,
