d5e7eae61876bdffcc0207f27f0ab2d21e3c8c68
Hayatullah Rahnamoon <rahnamoonhayatullah@gmail.com>
2019-02-10 14:25:49 +0800

    fixed error

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -2,24 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ServiceService } from '../services/service.service';
 import { Router } from '@angular/router';
-
+import _ from 'lodash'
 /**
  * @see Lodash_Components 
  */
-
-
-var _ = require('lodash');
-// Load the core build.
-var _ = require('lodash/core');
-// Load the FP build for immutable auto-curried iteratee-first data-last methods.
-var fp = require('lodash/fp');
-// Load method categories.
-var array = require('lodash/array');
-var object = require('lodash/fp/object');
-// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
-var at = require('lodash/at');
-var curryN = require('lodash/fp/curryN');
-
 
 
 @Component({
